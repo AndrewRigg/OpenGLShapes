728048d9a7ba29d110571b97413d7455c8270bcc
Andrew Rigg <AndrewJRigg@gmail.com>
2017-02-01 17:35:20 +0000

    Added GLM lib

---
+++ Project1/Project1/Physics.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <GLM/glm.hpp>
+#include <GLM/gtx/transform.hpp>
+
+#include "graphics.h"
+#include "shapes.h"
+
