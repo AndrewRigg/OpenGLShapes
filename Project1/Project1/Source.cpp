48a67b11c038ed0e2fb28fd4043bcaaa73ab6eb8
AndrewRigg <AndrewJRigg@gmail.com>
2017-02-05 23:33:29 +0000

    Tried to implement vectors=broken

--- Project1/Project1/Source.cpp
+++ Project1/Project1/Source.cpp
@@ -9,6 +9,7 @@
 // Suggestions or extra help please do email me S.Padilla@hw.ac.uk
 //
 // Note: Do not forget to link the libraries correctly and add the GLEW DLL in your debug/release folder.
+#define GFL
 
 #include <iostream>
 #include <vector>
@@ -37,10 +38,9 @@ void onResizeCallback(GLFWwindow* window, int w, int h);
 void onKeyCallback(GLFWwindow* window, int key, int scancode, int action, int mods);
 void explode();
 
-
 // VARIABLES
 bool		running = true;
-int const number_of_balls = 100;
+int const number_of_balls = 10;
 int const dimensions = 3;
 Graphics	myGraphics;		// Runing all the graphics in this object
 
@@ -48,8 +48,9 @@ INPUT_RECORD InputRecord;
 COORD coord;
 
 Cube		myCube;
-Sphere		mySphere, mySphere2;
+Sphere		mySphere;
 Sphere mySpheres[number_of_balls];
+vector <Sphere> spheres;
 Arrow		arrowX;
 Arrow		arrowY;
 Arrow		arrowZ;
@@ -58,50 +59,31 @@ float bounds[6];
 float t = 0.001f;					// Global variable for animation
 float g = -9.81f;					// Gravitational force
 float prevTime = glfwGetTime();	//Prev time
-Ball ball, ball2;
+Ball ball;
 vector<Ball> ball_vec;
 Ball balls[number_of_balls];
 
 int main()
 {
-
-	
-
-
 	srand(static_cast <unsigned> (time(0)));
 	int errorGraphics = myGraphics.Init();		// Launch window and graphics context
 	if (errorGraphics) return 0;				//Close if something went wrong...
 
-	bounds[0] = -3;
-	bounds[1] = 3;
-	bounds[2] = -3;
-	bounds[3] = 3;
-	bounds[4] = -3;
-	bounds[5] = 3;
-
-	//Random balls everywhere
-	/*for (int i = 0; i < number_of_balls; i++) {
-		balls[i].radius = 1;
-		balls[i].position = glm::vec3(-3 + static_cast <float> (rand()) / (static_cast <float> (RAND_MAX / (6))), -3 + static_cast <float> (rand()) / (static_cast <float> (RAND_MAX / (6))), -6.0f);
-		balls[i].velocity = glm::vec3(-3 + static_cast <float> (rand()) / (static_cast <float> (RAND_MAX / (6))), -3 + static_cast <float> (rand()) / (static_cast <float> (RAND_MAX / (6))), -3 + static_cast <float> (rand()) / (static_cast <float> (RAND_MAX / (6))));
-		balls[i].acceleration = glm::vec3(0.0f, 0.0f, 0.0f);
-	}*/
+	bounds[0] = -3.0f;
+	bounds[1] = 3.0f;
+	bounds[2] = -3.0f;
+	bounds[3] = 3.0f;
+	bounds[4] = -3.0f;
+	bounds[5] = 3.0f;
 
 	ball.radius = 1;
 	ball.setMass(27);
 	ball.position = glm::vec3(1.0f, 2.0f, -6.0f);
 	ball.velocity = glm::vec3(3.0f, 1.0f, 1.0f);
 	ball.acceleration = glm::vec3(0.0f, 0.0f, 0.0f);
-	//ball_vec.push_back(ball);
-
-	ball2.radius = 1;
-	ball2.position = glm::vec3(-1.0f, -2.0f, -6.0f);
-	ball2.velocity = glm::vec3(0.0f, 0.0f, 0.0f);
-	ball2.acceleration = glm::vec3(0.0f, 0.0f, 0.0f);
-	//ball_vec.push_back(ball2);
-	startup();									// Setup all necessary information for startup (aka. load texture, shaders, models, etc).
-	
-												// Mixed graphics and update functions - declared in main for simplicity.
+							
+	// Setup all necessary information for startup (aka. load texture, shaders, models, etc).						
+	// Mixed graphics and update functions - declared in main for simplicity.
 	glfwSetWindowSizeCallback(myGraphics.window, onResizeCallback);			// Set callback for resize
 	glfwSetKeyCallback(myGraphics.window, onKeyCallback);					// Set Callback for keys
 																			// MAIN LOOP run until the window is closed
@@ -114,19 +96,17 @@ int main()
 		glfwPollEvents();						// poll callbacks
 		update(currentTime);					// update (physics, animation, structures, etc)
 		render(currentTime);					// call render function.
-		for (int i = 0; i < ball_vec.size(); i++) {
-			//balls[i] = updatePhysics(balls[i], deltaTime);
-			balls[i].updatePhysics(deltaTime);
-		}
+		//for (int i = 0; i < ball_vec.size(); i++) {
+		//	//balls[i] = updatePhysics(balls[i], deltaTime);
+		//	ball_vec[i].updatePhysics(deltaTime);
+		//}
 		//ball = updatePhysics(ball, deltaTime);
 		ball.updatePhysics(deltaTime);
-		//ball2 = updatePhysics(ball2, deltaTime);
-		ball2.updatePhysics(deltaTime);
 		glfwSwapBuffers(myGraphics.window);		// swap buffers (avoid flickering and tearing)
 		prevTime = currentTime;
 
-		running &= (glfwGetKey(myGraphics.window, GLFW_KEY_ESCAPE) == GLFW_RELEASE);	// exit if escape key pressed
-		running &= (glfwWindowShouldClose(myGraphics.window) != GL_TRUE);
+		//running &= (glfwGetKey(myGraphics.window, GLFW_KEY_ESCAPE) == GLFW_RELEASE);	// exit if escape key pressed
+		//running &= (glfwWindowShouldClose(myGraphics.window) != GL_TRUE);
 	} while (running);
 
 	myGraphics.endProgram();			// Close and clean everything up...
@@ -139,27 +119,37 @@ int main()
 
 void explode() {
 	//Exploding balls from one point
-	for (int i = 0; i < number_of_balls; i++) {
-		balls[i].radius = 1;
-		balls[i].mass = 28;
-		balls[i].lifeTime = 100 + static_cast <float> (rand()) / (static_cast <float> (RAND_MAX / (1000)));
-		balls[i].position = ball.position;
-		//balls[i].velocity = glm::vec3(1.0f, 1.0f, 1.0f);
-		//balls[i].angular_velocity = glm::vec3(0.0f, 0.0f, 0.0f);
-		balls[i].angular_velocity = glm::vec3( - 1 + static_cast <float> (rand()) / (static_cast <float> (RAND_MAX / (2))), -1 + static_cast <float> (rand()) / (static_cast <float> (RAND_MAX / (2))), -1 + static_cast <float> (rand()) / (static_cast <float> (RAND_MAX / (2))));
-		balls[i].velocity = glm::vec3(-10 + static_cast <float> (rand()) / (static_cast <float> (RAND_MAX / (20))), -10 + static_cast <float> (rand()) / (static_cast <float> (RAND_MAX / (20))), -10 + static_cast <float> (rand()) / (static_cast <float> (RAND_MAX / (20))));
-		balls[i].acceleration = glm::vec3(0.0f, 0.0f, 0.0f);
 
-		ball_vec.push_back(balls[i]);
-		printf("Kinetic Energy: %f", balls[i].KineticEnergy());
-		printf(" Position: %f %f %f", balls[i].position.x, balls[i].position.y, balls[i].position.z);
-		printf(" Velocity: %f %f %f", balls[i].velocity.x, balls[i].velocity.y, balls[i].velocity.z);
-		printf(" Potential Energy: %f", balls[i].PotentialEnergy());
-		printf(" Mass: %f", balls[i].mass);
-		printf(" Alive: %d", balls[i].alive());
-		printf(" Momentum: %f %f %f", balls[i].Momentum().x, balls[i].Momentum().y, balls[i].Momentum().z);
-		printf("Lifetime: %f", balls[i].lifeTime);
-	}
+
+	//for (int i = 0; i < number_of_balls; i++) {
+	//	//Sphere 
+	//	mySpheres[i].Load();
+	//	mySpheres[i].fillColor = glm::vec4(static_cast <float> (rand()) / static_cast <float> (RAND_MAX), static_cast <float> (rand()) / static_cast <float> (RAND_MAX),
+	//		static_cast <float> (rand()) / static_cast <float> (RAND_MAX), static_cast <float> (rand()) / static_cast <float> (RAND_MAX));
+	//	spheres.push_back(mySpheres[i]);
+	//}
+
+	//for (int i = 0; i < number_of_balls; i++) {
+	//	balls[i].radius = 1;
+	//	balls[i].mass = 28;
+	//	balls[i].lifeTime = 100 + static_cast <float> (rand()) / (static_cast <float> (RAND_MAX / (1000)));
+	//	balls[i].position = ball.position;
+	//	//balls[i].velocity = glm::vec3(1.0f, 1.0f, 1.0f);
+	//	//balls[i].angular_velocity = glm::vec3(0.0f, 0.0f, 0.0f);
+	//	balls[i].angular_velocity = glm::vec3( - 1 + static_cast <float> (rand()) / (static_cast <float> (RAND_MAX / (2))), -1 + static_cast <float> (rand()) / (static_cast <float> (RAND_MAX / (2))), -1 + static_cast <float> (rand()) / (static_cast <float> (RAND_MAX / (2))));
+	//	balls[i].velocity = glm::vec3(-10 + static_cast <float> (rand()) / (static_cast <float> (RAND_MAX / (20))), -10 + static_cast <float> (rand()) / (static_cast <float> (RAND_MAX / (20))), -10 + static_cast <float> (rand()) / (static_cast <float> (RAND_MAX / (20))));
+	//	balls[i].acceleration = glm::vec3(0.0f, 0.0f, 0.0f);
+
+	//	ball_vec.push_back(balls[i]);
+	//	printf("Kinetic Energy: %f", balls[i].KineticEnergy());
+	//	printf(" Position: %f %f %f", balls[i].position.x, balls[i].position.y, balls[i].position.z);
+	//	printf(" Velocity: %f %f %f", balls[i].velocity.x, balls[i].velocity.y, balls[i].velocity.z);
+	//	printf(" Potential Energy: %f", balls[i].PotentialEnergy());
+	//	printf(" Mass: %f", balls[i].mass);
+	//	printf(" Alive: %d", balls[i].alive());
+	//	printf(" Momentum: %f %f %f", balls[i].Momentum().x, balls[i].Momentum().y, balls[i].Momentum().z);
+	//	printf("Lifetime: %f", balls[i].lifeTime);
+	//}
 }
 
 void startup() {
@@ -171,17 +161,15 @@ void startup() {
 	// Load Geometry
 	myCube.Load();
 
-	for (int i = 0; i < ball_vec.size(); i++) {
-		mySpheres[i].Load();
-		mySpheres[i].fillColor = glm::vec4(static_cast <float> (rand()) / static_cast <float> (RAND_MAX), static_cast <float> (rand()) / static_cast <float> (RAND_MAX), 
-			static_cast <float> (rand()) / static_cast <float> (RAND_MAX), static_cast <float> (rand()) / static_cast <float> (RAND_MAX));
-	}
+	//for (int i = 0; i < ball_vec.size(); i++) {
+	//	//Sphere 
+	//	spheres[i].Load();
+	//	spheres[i].fillColor = glm::vec4(static_cast <float> (rand()) / static_cast <float> (RAND_MAX), static_cast <float> (rand()) / static_cast <float> (RAND_MAX),
+	//		static_cast <float> (rand()) / static_cast <float> (RAND_MAX), static_cast <float> (rand()) / static_cast <float> (RAND_MAX));
+	//}
 
 	mySphere.Load();
 	mySphere.fillColor = glm::vec4(0.0f, 1.0f, 0.0f, 1.0f);	// You can change the shape fill colour, line colour or linewidth 
-
-	mySphere2.Load();
-	mySphere2.fillColor = glm::vec4(0.0f, 1.0f, 1.0f, 1.0f);
 
 	arrowX.Load(); arrowY.Load(); arrowZ.Load();
 	arrowX.fillColor = glm::vec4(1.0f, 0.0f, 0.0f, 1.0f); arrowX.lineColor = glm::vec4(1.0f, 0.0f, 0.0f, 1.0f);
@@ -203,18 +191,18 @@ void update(float currentTime) {
 	//myCube.mv_matrix = mv_matrix_cube;
 	//myCube.proj_matrix = myGraphics.proj_matrix;
 
-	vector <glm::mat4> mv_matrix_spheres_list;
+	//vector <glm::mat4> mv_matrix_spheres_list;
 
-	for (int i = 0; i < ball_vec.size(); i++) {
-		glm::mat4 mv_matrix_spheres =
-			glm::translate(ball_vec[i].position) *
-			glm::rotate(-t, ball_vec[i].angular_velocity) *
-			//glm::scale(glm::vec3(scale, scale, scale)) *		//trial
-			glm::mat4(1.0f);
-		mv_matrix_spheres_list.push_back(mv_matrix_spheres);
-		mySpheres[i].mv_matrix = mv_matrix_spheres_list[i];
-		mySpheres[i].proj_matrix = myGraphics.proj_matrix;
-	}
+	//for (int i = 0; i < ball_vec.size(); i++) {
+	//	glm::mat4 mv_matrix_spheres =
+	//		glm::translate(ball_vec[i].position) *
+	//		glm::rotate(-t, ball_vec[i].angular_velocity) *
+	//		//glm::scale(glm::vec3(scale, scale, scale)) *		
+	//		glm::mat4(1.0f);
+	//	mv_matrix_spheres_list.push_back(mv_matrix_spheres);
+	//	spheres[i].mv_matrix = mv_matrix_spheres_list[i];
+	//	spheres[i].proj_matrix = myGraphics.proj_matrix;
+	//}
 
 	// calculate Sphere movement
 	glm::mat4 mv_matrix_sphere =
@@ -225,12 +213,6 @@ void update(float currentTime) {
 		glm::mat4(1.0f);
 	mySphere.mv_matrix = mv_matrix_sphere; 
 	mySphere.proj_matrix = myGraphics.proj_matrix;
-
-	glm::mat4 mv_matrix_sphere2 =
-		glm::translate(ball2.position) *
-		glm::mat4(1.0f);
-	mySphere2.mv_matrix = mv_matrix_sphere2;
-	mySphere2.proj_matrix = myGraphics.proj_matrix;
 
 	//Calculate Arrows translations (note: arrow model points up)
 	//glm::mat4 mv_matrix_x =
@@ -260,10 +242,6 @@ void update(float currentTime) {
 	t += 0.01f; // increment movement variable
 }
 
-float update_vector(float derivative, float deltaTime) {
-	return derivative*deltaTime;
-}
-
 void render(float currentTime) {
 	// Clear viewport - start a new frame.
 	myGraphics.ClearViewport();
@@ -272,13 +250,12 @@ void render(float currentTime) {
 	//myCube.Draw();
 	//if (InputRecord.Event.MouseEvent.dwButtonState == FROM_LEFT_1ST_BUTTON_PRESSED) {
 	
-	
-		for (int i = 0; i < ball_vec.size(); i++) {
-			if (ball_vec[i].alive()) {
-				//printf("Lifetime: %f", balls[i].lifeTime);
-				mySpheres[i].Draw();
-			}
-	}
+	//for (int i = 0; i < ball_vec.size(); i++) {
+	//	if (ball_vec[i].alive()) {
+	//		//printf("Lifetime: %f", balls[i].lifeTime);
+	//		spheres[i].Draw();
+	//	}
+	//}
 	//}
 	mySphere.Draw();
 	//mySphere2.Draw();
@@ -298,7 +275,6 @@ void onResizeCallback(GLFWwindow* window, int w, int h) {	// call everytime the 
 void onKeyCallback(GLFWwindow* window, int key, int scancode, int action, int mods) { // called everytime a key is pressed
 	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
 		glfwSetWindowShouldClose(window, GLFW_TRUE);
-
 
 	if (key == GLFW_KEY_SPACE && action == GLFW_PRESS) {
 		explode();
